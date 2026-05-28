21d78d82bfa7c26d6cdf0302f4283b22495f86ef
Harmony.4 Health- Africa <harmony.4.health.h4hafrica@gmail.com>
2026-05-28 17:49:27 +0100

    Update Intro.js

--- js/Intro.js
+++ js/Intro.js
@@ -2,22 +2,12 @@ window.addEventListener("load", () => {
 
   const intro = document.getElementById("intro-screen");
 
-  // OPEN BOOK
   setTimeout(() => {
     intro.classList.add("open");
   }, 600);
 
-  // REMOVE INTRO
   setTimeout(() => {
-
-    intro.style.opacity = "0";
-
-    intro.style.transition = "opacity 1.5s ease";
-
-    setTimeout(() => {
-      intro.remove();
-    }, 1600);
-
+    intro.classList.add("hide");
   }, 5500);
 
 });
